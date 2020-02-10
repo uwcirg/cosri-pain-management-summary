57a04074dbb95f988673581fea09090bdd04003b
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-02-09 18:01:01 -0800

    Load SoF context from confidential back-end

--- src/launch.js
+++ src/launch.js
@@ -1,6 +1,9 @@
 import FHIR from 'fhirclient';
 
-fetch(`${process.env.PUBLIC_URL}/launch-context.json`)
+fetch(`${process.env.REACT_APP_CONF_API_URL}/auth/auth-info`, {
+  // include cookies in request
+  credentials: 'include'
+})
   .then((response)      => response.json())
   .then((launchContext) => FHIR.oauth2.authorize(launchContext))
   .catch((error)        => console.error(error));
