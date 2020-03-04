3ee64a05b94c69bd2be869a7a13f2570c7259c52
Amy Chen <amychen@amysbubble.local>
2020-03-04 09:06:01 -0800

    add missing change

--- src/launch.js
+++ src/launch.js
@@ -1,9 +1,13 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import FHIR from 'fhirclient';
 
 // retrieve launch context from backend, if configured
-let context_url = `${process.env.PUBLIC_URL}/launch-context.json`;
+// let context_url = `${process.env.PUBLIC_URL}/launch-context.json`;
+let context_url = process.env.PUBLIC_URL + '/launch-context.json';
 if (process.env.REACT_APP_CONF_API_URL){
-  context_url = `${process.env.REACT_APP_CONF_API_URL}/auth/auth-info`;
+  //context_url = `${process.env.REACT_APP_CONF_API_URL}/auth/auth-info`;
+  context_url = process.env.REACT_APP_CONF_API_URL+'/auth/auth-info';
 }
 
 fetch(context_url, {
@@ -11,5 +15,7 @@ fetch(context_url, {
   credentials: 'include'
 })
   .then((response)      => response.json())
-  .then((launchContext) => FHIR.oauth2.authorize(launchContext))
+  .then((launchContext) => {
+    console.log("context ", launchContext)
+    FHIR.oauth2.authorize(launchContext)})
   .catch((error)        => console.error(error));
