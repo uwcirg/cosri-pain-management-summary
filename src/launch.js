f8052deab5d2f6452fb9cb6d4bb5050cd449e70f
Amy Chen <amychen@amysbubble.local>
2020-03-04 16:20:47 -0800

    remove console.log statement

--- src/launch.js
+++ src/launch.js
@@ -3,10 +3,8 @@ import 'react-app-polyfill/stable';
 import FHIR from 'fhirclient';
 
 // retrieve launch context from backend, if configured
-// let context_url = `${process.env.PUBLIC_URL}/launch-context.json`;
 let context_url = process.env.PUBLIC_URL + '/launch-context.json';
 if (process.env.REACT_APP_CONF_API_URL){
-  //context_url = `${process.env.REACT_APP_CONF_API_URL}/auth/auth-info`;
   context_url = process.env.REACT_APP_CONF_API_URL+'/auth/auth-info';
 }
 
@@ -14,8 +12,8 @@ fetch(context_url, {
   // include cookies in request
   credentials: 'include'
 })
-  .then((response)      => response.json())
-  .then((launchContext) => {
-    console.log("context ", launchContext)
-    FHIR.oauth2.authorize(launchContext)})
-  .catch((error)        => console.error(error));
+.then((response)      => response.json())
+.then((launchContext) => {
+  FHIR.oauth2.authorize(launchContext);
+})
+.catch((error)        => console.error(error));
