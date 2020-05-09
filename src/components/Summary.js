e3e786f013ae6d9965a6bbf016a7efb323a1f922
Amy Chen <clone@uw.edu>
2020-05-08 17:08:45 -0700

    add no entries text

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -295,11 +295,11 @@ export default class Summary extends Component {
         <div className="panel-title">{panel.title}</div>
         <div className="stats-container">
           <div className="title">{panel.statsData.title}</div>
-          <div className="content">{statsContent}</div>
+          <div className="content">{statsContent || "No entries"}</div>
         </div>
         <div className="alerts-container">
           <div className="title">{panel.alertsData.title}</div>
-          <div className="content">{alertsContent}</div>
+          <div className="content">{alertsContent || "No alert entries found."}</div>
         </div>
       </div>)
   }
