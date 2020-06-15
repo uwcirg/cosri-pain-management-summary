69bf78ca07f202cad13515b7071a14c0c01af491
Amy Chen <clone@uw.edu>
2020-06-15 09:10:00 -0700

    use react polyfill library, remote dependency on deprecated babel polyfill lib

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
