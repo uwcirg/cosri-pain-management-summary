21bee03c4ec7b8f75c63eae9b977be062baee7e3
Ivan Cvitkovic <ivanc@uw.edu>
2020-06-19 00:42:41 -0700

    Use docker entrypoint script to write environment vars to js before startup

--- Dockerfile.production
+++ Dockerfile.production
@@ -10,4 +10,10 @@ RUN yarn build
 
 
 FROM nginx
+COPY docker-entrypoint.sh /usr/bin/docker-entrypoint.sh
 COPY --from=build-deps /opt/app/build /usr/share/nginx/html
+
+# write environment variables to config file and start
+ENTRYPOINT ["/usr/bin/docker-entrypoint.sh"]
+
+CMD ["nginx","-g","daemon off;"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -eu
+
+repo_path="$(cd "$(dirname "$0")" && pwd)"
+cmdname="$(basename "$0")"
+
+usage() {
+    cat << USAGE >&2
+Usage:
+    $cmdname command
+
+    Docker entrypoint script
+    Wrapper script that executes docker-related tasks before running given command
+
+USAGE
+    exit 1
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+    exit 0
+fi
+
+
+write_env_to_json() {
+    # write project-specific environment variables to app config file
+    local json_file="$1"
+
+    # only write environment variables containing "REACT_"
+    json_contents="{$(printenv | grep REACT_ | sed 's/^\|$/"/g' | sed 's|=|":"|' | paste -sd, -)}"
+    echo "$json_contents" > $json_file
+}
+
+
+write_env_to_json /usr/share/nginx/html/env.json
+
+
+echo $cmdname complete
+echo executing given command $@
+exec "$@"
