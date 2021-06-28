5378ada5c751cda3d07948d804c33ef9dc1c71ac
Amy Chen <clone@uw.edu>
2021-06-28 10:24:35 -0700

    rotate axis label

--- src/components/ExclusionBanner.js
+++ src/components/ExclusionBanner.js
@@ -31,7 +31,7 @@ export default class ExclusionBanner extends Component {
         <ChevronDownIcon className="close-button" icon="times" title="close" onClick={this.handleCloseToggle} width="25" height="25" />
 
         <div className="exclusion-banner__description">
-          <strong><FontAwesomeIcon icon="exclamation-circle" title="notice" /> LIMITATIONS</strong>
+          <strong className="title"><FontAwesomeIcon icon="exclamation-circle" title="notice" /> LIMITATIONS</strong>
           <p className="content">This guidance is <b><u>not intended</u></b> to apply to patients undergoing <b>end-of-life care (hospice or palliative)</b>, <b>inpatient treatment</b>, or <b>active cancer treatment</b>. However, some suggestions may be helpful in managing any patient.</p>
         </div>
       </div>
