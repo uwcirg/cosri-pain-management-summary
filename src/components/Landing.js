0fb75ef3f0b85ce8b2fc1ef480a22ad9469bafbe
Amy Chen <clone@uw.edu>
2021-08-24 15:10:46 -0700

    fix reference to system environment type

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -132,7 +132,7 @@ export default class Landing extends Component {
     });
   }
   saveSummaryData() {
-    if (String(getEnv("ENV")).toLowerCase() !== "development") return;
+    if (String(getEnv("REACT_APP_SYSTEM_TYPE")).toLowerCase() !== "development") return;
     const summary = this.state.result ? this.state.result.Summary : null;
     if (!summary) return;
     const patientName =  (summary&&summary.Patient?summary.Patient.Name:"");
