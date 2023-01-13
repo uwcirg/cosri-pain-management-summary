092d782a87ff3f6b4630ad1a5869e1b2fb65f66b
Amy Chen <amysbubble@Amys-MacBook-Pro-3.local>
2023-01-12 17:42:34 -0800

    responses styling fix, check for null score

--- src/components/ScoringSummary.js
+++ src/components/ScoringSummary.js
@@ -24,12 +24,16 @@ export default class ScoringSummary extends Component {
   getPreviousScore(data) {
     if (!data || data.length === 0 || data.length === 1) return null;
     const sortedData = this.sortData(data);
-    return parseInt(sortedData[1].score);
+    const score = sortedData[1].score;
+    if (!isNumber(score)) return null;
+    return parseInt(score);
   }
   getCurrentScore(data) {
     if (!data || data.length === 0) return null;
     const sortedData = this.sortData(data);
-    return parseInt(sortedData[0].score);
+    const score = sortedData[0].score;
+    if (!isNumber(score)) return null;
+    return parseInt(score);
   }
   getDisplayIcon(data) {
     const dataConfig = qConfig[data.QuestionnaireName];
