053ffd90607dc6969c2e5666bdbbd113e94ee80e
Amy Chen <amysbubble@Amys-MacBook-Pro-3.local>
2023-02-17 11:34:11 -0800

    lint fix

--- src/config/report_config.js
+++ src/config/report_config.js
@@ -21,8 +21,6 @@ const reportConfig = [
   {
     title: "Mental Health, Quality of Life and Sleep",
     dataKey: "mentalHealthSection",
-    subtitle:
-      "Depression (PHQ9), Anxiety (GAD-7), Post Traumatic Stress Disorder(PTSD), Quality of Life(QoL), Obstructive Sleep Apnea(STOP), Sleep initiation, and Sleep Maintenance",
     icon: (props) => (
       <UserIcon
         {...iconProps}
@@ -36,7 +34,20 @@ const reportConfig = [
         dataKey: "phq9",
         title: "PHQ-9 (Patient Health Questionnaire-9)",
         description:
-          "<p>PHQ-9 measures depression symptoms. A clinical diagnosis of depression should be confirmed by a clinician. Any positive response to suicidal ideation should be followed up immediately by a healthcare professional.</p><p>Depression is often associated with and can manifest as physical pain. Depression can also exacerbate physical pain. Treating depression may help to mitigate pain.</p><p>PHQ-9 scores and association with levels of depression:</p><table cellspacing='8'><tr><td>0</td><td>No depression</td></tr><tr><td>1-4</td><td>Minimal depression</td></tr><tr><td>5-9</td><td>Mild depression</td></tr><tr><td>10-14</td><td>Moderate depression</td></tr><tr><td>15-19</td><td>Moderately severe depression</td></tr><tr><td>20-27</td><td>Severe depression</td></tr></table>",
+          `<p>PHQ-9 measures depression symptoms. 
+          A clinical diagnosis of depression should be confirmed by a clinician. 
+          Any positive response to suicidal ideation should be followed up immediately by a healthcare professional.</p>
+          <p>Depression is often associated with and can manifest as physical pain. 
+          Depression can also exacerbate physical pain. 
+          Treating depression may help to mitigate pain.</p>
+          <p>PHQ-9 scores and association with levels of depression:</p>
+          <table cellspacing='8'>
+          <tr><td>0</td><td>No depression</td></tr>
+          <tr><td>1-4</td><td>Minimal depression</td></tr>
+          <tr><td>5-9</td><td>Mild depression</td></tr>
+          <tr><td>10-14</td><td>Moderate depression</td></tr>
+          <tr><td>15-19</td><td>Moderately severe depression</td></tr>
+          <tr><td>20-27</td><td>Severe depression</td></tr></table>`,
         component: (props) => <ResponsesSummary {...props}></ResponsesSummary>,
       },
       {
@@ -44,7 +55,15 @@ const reportConfig = [
         dataKey: "gad7",
         title: "GAD-7 (General Anxiety Disorder-7)",
         description:
-          "<p>The <b>Generalized Anxiety Disorder Assessment</b> (GAD-7) is a seven-item instrument that is used to measure or assess the severity of generalized anxiety disorder (GAD). Each item asks the individual to rate the severity of his or her symptoms over the past two weeks.</p><p>GAD-7 scores and association with levels of anxiety:</p><table cellspacing='8'><tr><td>0-4</td><td>Minimal anxiety</td></tr><tr><td>5-9</td><td>Mild anxiety</td></tr><tr><td>10-14</td><td>Moderate anxiety</td></tr><tr><td>15-21</td><td>Severe anxiety</td></tr></table>",
+          `<p>The <b>Generalized Anxiety Disorder Assessment</b> (GAD-7) is a seven-item instrument 
+          that is used to measure or assess the severity of generalized anxiety disorder (GAD). 
+          Each item asks the individual to rate the severity of his or her symptoms over the past two weeks.</p>
+          <p>GAD-7 scores and association with levels of anxiety:</p>
+          <table cellspacing='8'>
+          <tr><td>0-4</td><td>Minimal anxiety</td></tr>
+          <tr><td>5-9</td><td>Mild anxiety</td></tr>
+          <tr><td>10-14</td><td>Moderate anxiety</td></tr>
+          <tr><td>15-21</td><td>Severe anxiety</td></tr></table>`,
         component: (props) => <ResponsesSummary {...props}></ResponsesSummary>,
       },
     ],
