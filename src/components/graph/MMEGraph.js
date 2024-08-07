93af0ba0ade492f2d87ce8da3cde0a2aa53a0b40
Amy Chen <amysbubble@MacBook-Pro-5.local>
2024-08-07 14:07:06 -0700

    add CDC related text changes

--- src/components/graph/MMEGraph.js
+++ src/components/graph/MMEGraph.js
@@ -133,7 +133,7 @@ export default class MMEGraph extends Component {
     const parentHeight = 344;
     const WA_MAX_VALUE = 120;
     const CDC_SECONDARY_MAX_VALUE = 50;
-    const CDC_MAX_VALUE = 90;
+    //const CDC_MAX_VALUE = 90;
     const xIntervals = 12;
     let lineParamsSet = [xIntervals, xFieldName, yFieldName];
     const hasError = this.props.error;
@@ -213,7 +213,7 @@ export default class MMEGraph extends Component {
 
     let WAData = this.getDefaultDataValueSet(WA_MAX_VALUE, baseLineDate, maxDate, ...lineParamsSet);
     let CDCSecondaryData = this.getDefaultDataValueSet(CDC_SECONDARY_MAX_VALUE, baseLineDate, maxDate, ...lineParamsSet);
-    let CDCData = this.getDefaultDataValueSet(CDC_MAX_VALUE, baseLineDate, maxDate, ...lineParamsSet);
+    //let CDCData = this.getDefaultDataValueSet(CDC_MAX_VALUE, baseLineDate, maxDate, ...lineParamsSet);
 
     const margins = {
       top: 8,
@@ -328,10 +328,10 @@ export default class MMEGraph extends Component {
               <Line lineID="dataLine" data={data} {...dataLineProps} />
               <Line lineID="WALine" strokeColor={WA_COLOR} dotted="true" dotSpacing="3, 3" data={WAData} {...defaultProps} />
               <Line lineID="CDCSecondaryLine" strokeColor={CDC_COLOR} dotted="true" dotSpacing="3, 3" data={CDCSecondaryData} {...defaultProps} />
-              <Line lineID="CDCLine" strokeColor={CDC_COLOR} dotted="true" dotSpacing="3, 3" data={CDCData} {...defaultProps} />
-              <text {...WALegendSettings}>Washington State consultation threshold</text>
-              <text {...CDCLegendSettings} y={yScale(50 + textMargin)}>CDC extra precautions threshold</text>
-              <text {...CDCLegendSettings} y={yScale(90 + textMargin)}>CDC avoid/justify threshold</text>
+              {/* <Line lineID="CDCLine" strokeColor={CDC_COLOR} dotted="true" dotSpacing="3, 3" data={CDCData} {...defaultProps} /> */}
+              <text {...WALegendSettings}>WA State: Consultation threshold</text>
+              <text {...CDCLegendSettings} y={yScale(50 + textMargin)}>CDC: Consider offering naloxone</text>
+              {/* <text {...CDCLegendSettings} y={yScale(90 + textMargin)}>CDC avoid/justify threshold</text> */}
             </g>
           </svg>
         </div>
