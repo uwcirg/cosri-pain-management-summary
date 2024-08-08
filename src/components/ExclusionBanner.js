0a64c343645332a09df9842cde15cd3989049b76
Amy Chen <achen2401@gmail.com>
2024-08-08 09:43:29 -0700

    add CDC related text changes (#155)
    Co-authored-by: Amy Chen <amysbubble@MacBook-Pro-5.local>

--- src/components/ExclusionBanner.js
+++ src/components/ExclusionBanner.js
@@ -6,12 +6,13 @@ export default class ExclusionBanner extends Component {
   constructor() {
     super(...arguments);
     this.state = {
-      displayed: true
+      displayed: false
     };
     this.handleCloseToggle= this.handleCloseToggle.bind(this);
   }
 
-  handleClose = () => {
+  handleClose = (e) => {
+    e.stopPropagation();
     this.setState({ displayed: false });
   }
 
@@ -28,11 +29,29 @@ export default class ExclusionBanner extends Component {
       <div
         className={`exclusion-banner banner ${conditionalClass}`}
         role="banner"
-        onClick={this.handleCloseToggle}>
+        onClick={this.handleCloseToggle}
+        onKeyDown={this.handleCloseToggle}
+      >
         <ChevronDownIcon className="close-button" icon="times" title="close"  width="25" height="25" />
 
         <div className="exclusion-banner__description">
-          <strong className="title"><FontAwesomeIcon icon="exclamation-circle" title="notice" /> LIMITATIONS</strong> <span className="content">Guidance <b>not intended</b> for <b>palliative</b>, <b>inpatient</b>, or <b>active cancer care</b>.</span>
+          <strong className="title"><FontAwesomeIcon icon="exclamation-circle" title="notice" /> LIMITATIONS</strong> Guidance for adult pain except... <span className="info-icon" role="button">(click here)</span>.
+          <div className="content">
+            <p>CDC's 2022 Clinical Practice Guideline <strong>applies</strong> to outpatients aged 18 years and older with:</p>
+            <ul>
+                <li>Acute pain (duration less than 1 month)</li>
+                <li>Subacute pain (duration of 1-3 months)</li>
+                <li>Chronic pain (duration of 3 months or more)</li>
+            </ul>
+            <p>It does <strong>not</strong> apply to management of patients</p>
+            <ul>
+                <li>with Sickle cell disease</li>
+                <li>with Cancer-related pain</li>
+                <li>receiving Palliative care</li>
+                <li>receiving End-of-life care</li>
+            </ul>
+            <div class="text-right"><button className="plain info-icon" onClick={this.handleClose}>Click to hide info</button></div>
+          </div>
         </div>
       </div>
     );
